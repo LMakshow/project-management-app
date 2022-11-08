e244fc52c694697e928f9ac1d0536fd19ec69189
Maksym Lytvyn <makshow.at@gmail.com>
2022-11-08 23:28:32 +0200

    feat: add dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,21 @@
 import styles from './layout.module.css'
+import { useTheme as useNextTheme } from 'next-themes'
+import { Switch, useTheme } from '@nextui-org/react'
 
 export default function Navbar() {
-  return <header className={styles.header}>Navbar</header>
+  const { setTheme } = useNextTheme()
+  const { isDark, type } = useTheme()
+
+  return (
+    <header className={styles.header}>
+      <p>Navbar</p>
+      <div>
+        The current theme is: {type}
+        <Switch
+          checked={isDark}
+          onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+        />
+      </div>
+    </header>
+  )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,33 @@
-import { NextUIProvider } from '@nextui-org/react'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+
+const lightTheme = createTheme({
+  type: 'light',
+  theme: {
+    colors: {},
+  },
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {},
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme='system'
+      attribute='class'
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
   )
 }
