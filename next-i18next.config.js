e2bbab70049811d28e7304333dd79cb30fd17a7f
Maksym Lytvyn <makshow.at@gmail.com>
2022-11-14 11:54:48 +0200

    fix: localepath config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
-    localePath: path.resolve('./public/locales')
   },
+  localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development'
 };
