95acc1844cca0a051938eacc8c467d51673abc4d
Maksym Lytvyn <makshow.at@gmail.com>
2022-11-16 21:02:43 +0200

    feat: start rtqk implementation of boards api

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -25,3 +25,16 @@ export interface UserSignInResponse {
     status: number
   }
 }
+
+export interface BoardRequest {
+  title: string
+  owner: string
+  users: string[]
+}
+
+export interface BoardResponse {
+  _id: string
+  title: string
+  owner: string
+  users: string[]
+}
